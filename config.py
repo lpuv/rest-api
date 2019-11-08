bc8e8b4706785ce7abf3b10e6106fd11580219c5
Gamer4life <lpuvilla0001@mymail.lausd.net>
2019-11-08 18:44:09 +0000

    Done with part 2

--- config.py
+++ config.py
@@ -5,18 +5,21 @@ from flask_marshmallow import Marshmallow
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Create the Connexion application instance
+# Create the connexion application instance
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 # Get the underlying Flask app instance
 app = connex_app.app
 
-# Configure the SQLAlchemy part of the app instance
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'people.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Build the Sqlite ULR for SqlAlchemy
+sqlite_url = "sqlite:////" + os.path.join(basedir, "people.db")
 
-# Create the SQLAlchemy db instance
+# Configure the SqlAlchemy part of the app instance
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = sqlite_url
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Create the SqlAlchemy db instance
 db = SQLAlchemy(app)
 
 # Initialize Marshmallow
